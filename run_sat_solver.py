c827daa87be37355d6232e74029632bd840d04aa
lovishchopra <lovish@sh03-12n08.int>
2023-12-05 16:55:58 -0800

    Changes to counter example prompt and run sat solver

--- run_sat_solver.py
+++ run_sat_solver.py
@@ -1,22 +1,23 @@
 import subprocess
+import sys
 
-def run_cvc5(smt2_file):
+def run_cvc5(smt2_file_path, output_text_file_path):
     try:
         # Run CVC5 and capture output
-        result = subprocess.run(["cvc5", smt2_file], capture_output=True, text=True, check=True)
+        result = subprocess.run(["cvc4", smt2_file_path], capture_output=True, text=True, check=True)
 
-        # Print the output
-        print("CVC5 Output:")
-        print(result.stdout)
-    
+        with open(output_text_file_path, "w") as f:
+            f.write(result.stdout)
+
     except subprocess.CalledProcessError as e:
-        print("Error running CVC5", e.stderr)
+        print("Error running CVC4", e.stderr)
 
 if __name__ == "__main__":
     # Check if a file path is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python run_sat_solver.py <smt2_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python run_sat_solver.py <smt2_file> <output_text_file>")
         sys.exit(1)
 
     smt2_file_path = sys.argv[1]
-    run_cvc5(smt2_file_path)
+    output_text_file_path = sys.argv[2]
+    run_cvc5(smt2_file_path, output_text_file_path)
