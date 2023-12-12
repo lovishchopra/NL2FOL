ffdb8da3d0366ac23df4cfc4387a8a55dd4271bb
Lovish Chopra <lovishchopra98@gmail.com>
2023-12-12 13:15:53 -0800

    Update fol_to_cvc.py

--- fol_to_cvc.py
+++ fol_to_cvc.py
@@ -27,6 +27,20 @@ if __name__ == "__main__":
                 if len(proc_result) == 0:
                     results.append("")
                 result, _ = proc_result.split("\n", 1)
+                
+                if "unknown" in result:
+                    script = CVCGenerator(fol[i].replace("ForAll", "forall").replace("ThereExists", "exists").replace("&", "and").replace("~", "not ")).generateCVCScript(finite_model_finding=True)
+                    with open("results/run7_smt/{0}.smt2".format(i), "w") as f:
+                        f.write(script)
+                    with open("results/run7_smt/{0}_out.txt".format(i), "w") as f:
+                        # Run CVC5 and capture output
+                        proc = subprocess.run(["cvc4", "results/run4_smt/{0}.smt2".format(i)], capture_output=True, text=True, check=True)
+                        proc_result = proc.stdout
+                        f.write(proc_result)
+                        if len(proc_result) == 0:
+                            results.append("")
+                        result, _ = proc_result.split("\n", 1)
+                        
                 if "unsat" in result:
                     results.append("Valid")
                 elif "unknown" in result or "sat" in result:
@@ -35,7 +49,7 @@ if __name__ == "__main__":
                     results.append("")
 
         except:
-            print("HERE")
+            print("Cannot run this statement")
             results.append("")
             pass
     data['result'] = results
