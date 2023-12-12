9d02a20135426378ff4e7379b93e9b8f840d74ff
labhinav <abhinavlalwani@gmail.com>
2023-12-12 15:00:34 -0800

    add results for both classes

--- get_metrics.py
+++ get_metrics.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score
 filename='results/run7_results.csv'
+def get_results(label,preds):
+    acc=accuracy_score(label[label==1],preds[label==1])
+    prec=precision_score(label,preds)
+    rec=recall_score(label,preds)
+    f1=f1_score(label,preds)
+    return acc, prec, rec, f1
+
 if __name__=='__main__':
     df=pd.read_csv(filename)
     label=1-df['label']
     preds=pd.Categorical(df['result'],categories=['Valid','LF']).codes
     preds = np.where(preds == -1, 1, preds)
-    acc=accuracy_score(label,preds)
-    prec=precision_score(label,preds)
-    rec=recall_score(label,preds)
-    f1=f1_score(label,preds)
-    print(acc, prec, rec, f1)
+    print(get_results(label,preds))
+    print(get_results(1-label,1-preds))
