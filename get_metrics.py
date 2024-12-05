4db7b98bde03b53e737576ad1ad9af8eafd8c108
Tasha Kim <tashakim@google.com>
2024-12-05 05:43:33 +0000

    Refactor get_metrics.py remove redundant indexing for rows

--- get_metrics.py
+++ get_metrics.py
@@ -1,7 +1,10 @@
-import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score
-filename='results/few_shot_claude_combined_results.csv'
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+filename='results/zero_shot_gpt-4o_climate_run_results.csv'
+
 def get_results(label,preds):
     acc=accuracy_score(label,preds)
     prec=precision_score(label,preds)
@@ -11,9 +14,7 @@ def get_results(label,preds):
 
 if __name__=='__main__':
     df=pd.read_csv(filename)
-    df=df[201:400]
     label=1-df['label']
-    # preds=pd.Categorical(df['result'],categories=['Valid','LF']).codes
     preds=1-df['result']
     preds = np.where(preds == -1, 1, preds)
     print(get_results(label,preds))
