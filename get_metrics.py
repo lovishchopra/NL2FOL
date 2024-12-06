828a326a4883e51cea64990f1cc6a526081dfec7
Tasha Kim <tashakim@google.com>
2024-12-06 00:25:21 +0000

    Comment out redundant metrics output in get_metrics

--- get_metrics.py
+++ get_metrics.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
-filename='results/zero_shot_gpt-4o_climate_run_results.csv'
+filename='results/output_file_name.csv'
 
 def get_results(label,preds):
     acc=accuracy_score(label,preds)
@@ -18,4 +18,4 @@ if __name__=='__main__':
     preds=1-df['result']
     preds = np.where(preds == -1, 1, preds)
     print(get_results(label,preds))
-    print(get_results(1-label,1-preds))
+    # print(get_results(1-label,1-preds))
