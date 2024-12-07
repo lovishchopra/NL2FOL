206ad47d26c1dd2722f8cb371a6d581485eeba11
Tasha Kim <tashakim@google.com>
2024-12-07 05:42:21 +0000

    Add few shot results for claude 3.5 on logic and logicclimate

--- few_shot_anthropic.py
+++ few_shot_anthropic.py
@@ -1,35 +1,45 @@
-import pandas as pd
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import transformers
-import torch
 import anthropic
-client = anthropic.Anthropic()
-df=pd.read_csv('results/combined_results.csv')
-df=df[0:400]
+import pandas as pd
+import torch
+import transformers
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+client = anthropic.Client(api_key="your_api_key")
+
+model = "gpt-4o"  # [gpt-4o, gpt-4o-mini, o1-preview, o1-mini]
+method = "few_shot"  # [zero_shot, few_shot, few_shot_cot]
+input_file = "climate_run_results"  # [logic_run_results, climate_run_results]
+num_rows = 200
+
+df = pd.read_csv(f"results/{input_file}.csv")
+df=df[0:num_rows]
 results=[]
-explanations=[]
+
 for i,row in df.iterrows():
-    if isinstance(row['articles'],float):
+    if isinstance(row['articles'], float):
         results.append(0)
         continue
-    with open("prompts/prompt_few_shot.txt", encoding="ascii", errors="ignore") as f:
+    with open(f"prompts/prompt_{method}.txt", encoding="ascii", errors="ignore") as f:
         prompt = f.read() + row['articles'] + " \nAnswer: "
-    message = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0.0,
-        messages=[
-            {"role": "user", "content": prompt},
-        ]
-    )
-    result=message.content[0].text
+    try:
+        message = client.completions.create(
+            model="claude-3.5-sonnet",
+            max_tokens=1000,
+            temperature=0.0,
+            prompt=f"{anthropic.HUMAN_PROMPT}{prompt}{anthropic.AI_PROMPT}",
+        )
+        result=message.get('completion', '').strip()
+    except:
+        result="500 ERROR RESULT"
     print(result)
-    if result.strip().startswith("V"):
+    if "Valid" in result.strip():
         results.append(1)
         print(1)
     else:
         results.append(0)
         print(0)
     print(result)
+
 df['result']=results
-df.to_csv('results/few_shot_claude_combined_results.csv')
+df.to_csv(f'results/{method}_{model}_{input_file}.csv')
